4dbe9fd780c1fc182d0d5119f9e8f3ae132232ac
Mateus Sampaio <mateus.sampaio@kbrtec.com.br>
2025-03-24 17:32:20 -0300

    up: version java and add swagger in project

--- src/main/java/com/medicalsystem/MedicalSystemApplication.java
+++ src/main/java/com/medicalsystem/MedicalSystemApplication.java
@@ -1,8 +1,12 @@
 package com.medicalsystem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Medical System Api", version = "1", description = ""), servers = { @Server(url = "/")})
 @SpringBootApplication
 public class MedicalSystemApplication {
 
